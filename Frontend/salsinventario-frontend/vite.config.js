f8cd28c25220bbff33a5fbb2f9b1722bc9f372e4
bryanstithquintero <bryanquinterog@gmail.com>
2023-09-07 18:02:02 -0500

    add product search and sidebar

--- Frontend/salsinventario-frontend/vite.config.js
+++ Frontend/salsinventario-frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const customJsxLoader = {
+  name: 'custom-jsx-loader',
+  transform(code, id) {
+    if (id.endsWith('.js')) {
+      return {
+        code: code.replace(/\.js$/, '.jsx'), // Cambia la extensión a ".jsx"
+        map: null,
+      };
+    }
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), customJsxLoader],
 })
